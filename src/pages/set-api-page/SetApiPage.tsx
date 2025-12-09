c0b03f9bc9c2392ae6981b0ce1c64ddb2e7c68d0
LorenzoParolini <lorenzo.parolini@itstechtalentfactory.it>
2025-12-09 22:43:07 +0100

    style: sostituiti alert con messaggi UI

--- src/pages/set-api-page/SetApiPage.tsx
+++ src/pages/set-api-page/SetApiPage.tsx
@@ -11,38 +11,43 @@ interface SetApiPageProps {
 
 function SetApiPage({ onApiKeySaved }: SetApiPageProps) {
 
-  
-const setApiKey = useAPIStore((state) => state.setAPI);
-const [inputValue, setInputValue] = useState('');
 
-const isValidFormat = (key: string) => {
-  return /^[a-zA-Z0-9]{32}$/.test(key);
-};
+  const setApiKey = useAPIStore((state) => state.setAPI);
+  const [inputValue, setInputValue] = useState('');
 
-const handleSubmit = (e: React.FormEvent) => {
-  if (isValidFormat(inputValue)) {
-    e.preventDefault();
-    setApiKey(inputValue);
-    onApiKeySaved();
-  } else {
-    e.preventDefault();
-    alert("Invalid API key format. \nPlease enter a valid 32-character alphanumeric key.");
-  }
-};
+
+  const isValidFormat = (key: string) => {
+    return /^[a-zA-Z0-9]{32}$/.test(key);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    if (isValidFormat(inputValue)) {
+      e.preventDefault();
+      setApiKey(inputValue);
+      onApiKeySaved();
+    } else {
+      e.preventDefault();
+    }
+  };
 
   return (
     <div className="api-page-container">
-     <h1 className="api-page-title">Welcome on IamChef</h1>
-     <form onSubmit={handleSubmit} className="api-form">
-      <input 
-        type="text" 
-        value={inputValue} 
-        onChange={(e) => setInputValue(e.target.value)}
-        placeholder="To start, enter your API key here..." 
-        className="api-input"
-      />
-      <button type="submit" className="api-submit-button">Save & Continue</button>
-     </form>
+      <h1 className="api-page-title">Welcome on IamChef</h1>
+      <form onSubmit={handleSubmit} className="api-form">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="To start, enter your API key here..."
+          className="api-input"
+        />
+        {
+          !isValidFormat(inputValue) && inputValue.length > 0 && (
+            <p className="api-error-message">Invalid API key format. Please enter a valid 32-character alphanumeric key.</p>
+          )
+        }
+        <button type="submit" className="api-submit-button">Save & Continue</button>
+      </form>
     </div>
   );
 }
